bc2838383d2f8b435aa2b70af4bf486eb65db0fe
Hamid <hamidjaved615@gmail.com>
2024-08-21 21:23:37 +0500

    Fix some erros in register and login components nd added the API and they are successfully working

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -15,49 +15,49 @@ const Categories = () => {
                     <button className='border-[1.5px] border-black p-3 rounded-3xl hover:bg-[#0DAFE6] hover:text-white hover:border-none'>All Categories</button>
                 </div>
                 <div className="flex flex-wrap justify-center gap-10 cursor-pointer">
-                    <div className=" w-[20%] h-48 border-[1px] border-slate-400 flex rounded-lg flex-col gap-4 justify-center items-center hover:scale-105 hover:shadow-md hover:shadow-[#0DAFE6]">
+                    <div className=" w-[20%] h-48 border-[1px] border-slate-400 flex rounded-lg flex-col gap-4 justify-center items-center hover:scale-105 hover:shadow-md transition hover:shadow-[#0DAFE6]">
                         <CgWebsite className='text-4xl text-[#0DAFE6]' />
                         <div className="">
                             <h3 className='font-semibold '>Web Development</h3>
                             <p className='text-center'>15 Courses</p>
                         </div>
                     </div>
-                    <div className=" w-[20%] h-48 border-[1px] border-slate-400 flex rounded-lg flex-col gap-4 justify-center items-center hover:scale-105 hover:shadow-md hover:shadow-[#0DAFE6]">
+                    <div className=" w-[20%] h-48 border-[1px] border-slate-400 flex rounded-lg flex-col gap-4 justify-center items-center hover:scale-105 hover:shadow-md transition hover:shadow-[#0DAFE6]">
                         <FaPalette className='text-4xl text-[#0DAFE6]' />
                         <div className="">
                             <h3 className='font-semibold'>Ui/Ux Design</h3>
                             <p className='text-center'>19 Courses</p>
                         </div>
                     </div>
-                    <div className=" w-[20%] h-48 border-[1px] border-slate-400 flex rounded-lg flex-col gap-4 justify-center items-center hover:scale-105 hover:shadow-md hover:shadow-[#0DAFE6]">
+                    <div className=" w-[20%] h-48 border-[1px] border-slate-400 flex rounded-lg flex-col gap-4 justify-center items-center hover:scale-105 hover:shadow-md transition hover:shadow-[#0DAFE6]">
                         <FaChartPie className='text-4xl text-[#0DAFE6]' />
                         <div className="">
                             <h3 className='font-semibold'>Data Science</h3>
                             <p className='text-center'>22 Courses</p>
                         </div>
                     </div>
-                    <div className=" w-[20%] h-48 border-[1px] border-slate-400 flex rounded-lg flex-col gap-4 justify-center items-center hover:scale-105 hover:shadow-md hover:shadow-[#0DAFE6]">
+                    <div className=" w-[20%] h-48 border-[1px] border-slate-400 flex rounded-lg flex-col gap-4 justify-center items-center hover:scale-105 hover:shadow-md transition hover:shadow-[#0DAFE6]">
                         <FaCode className='text-4xl text-[#0DAFE6]' />
                         <div className="">
                             <h3 className='font-semibold'>DevOps</h3>
                             <p className='text-center'>8 Courses</p>
                         </div>
                     </div>
-                    <div className=" w-[20%] h-48 border-[1px] border-slate-400 flex rounded-lg flex-col gap-4 justify-center items-center hover:scale-105 hover:shadow-md hover:shadow-[#0DAFE6]">
+                    <div className=" w-[20%] h-48 border-[1px] border-slate-400 flex rounded-lg flex-col gap-4 justify-center items-center hover:scale-105 hover:shadow-md transition hover:shadow-[#0DAFE6]">
                         <FaPaintbrush className='text-4xl text-[#0DAFE6]' />
                         <div className="">
                             <h3 className='font-semibold'>Graphics Design</h3>
                             <p className='text-center'>17 Courses</p>
                         </div>
                     </div>
-                    <div className=" w-[20%] h-48 border-[1px] border-slate-400 flex rounded-lg flex-col gap-4 justify-center items-center hover:scale-105 hover:shadow-md hover:shadow-[#0DAFE6]">
+                    <div className=" w-[20%] h-48 border-[1px] border-slate-400 flex rounded-lg flex-col gap-4 justify-center items-center hover:scale-105 hover:shadow-md transition hover:shadow-[#0DAFE6]">
                         <FaBackward className='text-4xl text-[#0DAFE6]' />
                         <div className="">
                             <h3 className='font-semibold'>Backend Develo...</h3>
                             <p className='text-center'>25 Courses</p>
                         </div>
                     </div>
-                    <div className=" w-[20%] h-48 border-[1px] border-slate-400 flex rounded-lg flex-col gap-4 justify-center items-center hover:scale-105 hover:shadow-md hover:shadow-[#0DAFE6]">
+                    <div className=" w-[20%] h-48 border-[1px] border-slate-400 flex rounded-lg flex-col gap-4 justify-center items-center hover:scale-105 hover:shadow-md transition hover:shadow-[#0DAFE6]">
                         <FaFingerprint className='text-4xl text-[#0DAFE6]' />
                         <div className="">
                             <h3 className='font-semibold'>Cyber Security</h3>
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,20 +1,45 @@
 import React, { useRef } from "react";
 import useClickOutside from "../Hooks/UseClickOutside";
 
-const Login = ({setShowLogin}) => {
+const Login = ({ setShowLogin }) => {
 
     const boxRef = useRef(null)
-    useClickOutside (boxRef, () => setShowLogin(false))
+    useClickOutside(boxRef, () => setShowLogin(false))
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const obj = Object.fromEntries(formData.entries());
+        console.log(obj);
+        console.log(2)
+
+        try {
+            const response = await fetch('https://elearningportal-56538109f664.herokuapp.com/auth/login ', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(obj),
+            });
+
+            const result = response.status;
+            console.log('Response from API:', result);
+        } catch (error) {
+            console.error('Error sending data:', error);
+        }
+    };
+
     return (
         <>
-           <div  className="bg-black bg-opacity-45 w-screen fixed top-0 left-0 flex flex-col justify-center h-screen">
-                <div ref={boxRef} className="w-full max-w-md mx-auto p-4 bg-white rounded shadow-md">
+            <div className="bg-black bg-opacity-45 w-screen fixed top-0 left-0 flex flex-col justify-center h-screen">
+                <form ref={boxRef} onSubmit={handleSubmit} className="w-full max-w-md mx-auto p-8 bg-white rounded shadow-md">
                     <div className="flex justify-start mb-4">
                         <b className="text-2xl">Login</b>
                     </div>
                     <div>
                         <input
-                            type="text"
+                            name="email"
+                            type="email"
                             placeholder="Email or Username*"
                             required
                             className="w-full p-2 outline-none  text-sm text-gray-700 mt-5 border border-white border-b-2 border-b-black"
@@ -22,7 +47,8 @@ const Login = ({setShowLogin}) => {
                     </div>
                     <div>
                         <input
-                            type="text"
+                            name="password"
+                            type="password"
                             placeholder="Password*"
                             required
                             className="w-full p-2 outline-none  text-sm text-gray-700 mt-5 border border-white border-b-2 border-b-black"
@@ -40,12 +66,12 @@ const Login = ({setShowLogin}) => {
                         </label>
                     </div>
                     <input
-                        className="w-full p-2 bg-[#0DAFE6] hover:bg-[white] hover:text-[#0DAFE6] text-white font-bold py-2 px-4 rounded-xl mt-6"
-                        type="button"
+                        className="w-full cursor-pointer p-2 bg-[#0DAFE6] hover:bg-[white] hover:text-[#0DAFE6] active:bg-[#0DAFE6] active:text-white text-white font-bold py-2 px-4 rounded-xl mt-6"
+                        type="submit"
                         value="Login"
                     />
-                    <p className="text-sm text-gray-700 mt-3">Forget Password?</p>
-                </div>
+                    <p className="text-sm cursor-pointer text-gray-700 mt-3">Forget Password?</p>
+                </form>
             </div>
         </>
     );
