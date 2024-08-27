3dec017b88c5c9fe255ec5daef5c36fca74d84a5
Naeem Ashraf <123843315+NP5555@users.noreply.github.com>
2024-08-27 11:44:23 -0700

    "Added footer Final update

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -6,7 +6,7 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-100 w-full h-1/4 px-4 py-4 mt-3">
+    <footer className="bg-gray-100 p-4 w-full h-1/4 px-4 py-4 mt-3">
       <div className="container mx-auto flex  flex-col lg:flex-row justify-between items-start lg:items-center">
         <div className="mb-4 lg:mb-0 mx-10 items-center">
           <div className="logo flex justify-start items-start">
