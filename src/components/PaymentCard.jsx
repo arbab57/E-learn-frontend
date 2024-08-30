ecd5d81e89d7614eb3826bbc4cf38395a868c30a
Hamid <hamidjaved615@gmail.com>
2024-08-30 15:48:53 +0500

    Completees

--- src/components/PaymentCard.jsx
+++ src/components/PaymentCard.jsx
@@ -5,44 +5,69 @@ import { URLS } from "../data/remote/URL";
 import Toast from "./General/Toast";
 import Loader from "./General/Loader";
 
-
-
 const PaymentCard = ({ courseId, course, setShowBuy, setBuy }) => {
-  console.log(courseId)
-
   const modalRef = useRef(null);
   const onClose = () => {
     setShowBuy(false);
   };
   useClickOutside(modalRef, onClose);
 
-  const [res, setRes] = useState("")
-  const [severity, setSeverity] = useState("")
-  const [showToast, setShowToast] = useState(false)
-  const [loading, setLoading] = useState(false)
-
+  const [cardNumber, setCardNumber] = useState("");
+  const [cardName, setCardName] = useState("");
+  const [expiryDate, setExpiryDate] = useState("");
+  const [cvv, setCvv] = useState("");
+  const [errors, setErrors] = useState({});
+  const [res, setRes] = useState("");
+  const [severity, setSeverity] = useState("");
+  const [showToast, setShowToast] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setLoading(true)
+
+    // Validate form inputs
+    const formErrors = {};
+    if (!cardNumber) formErrors.cardNumber = "Card number is required";
+    else if (cardNumber.length !== 16)
+      formErrors.cardNumber = "Card number must be 16 digits";
+
+    if (!cardName) formErrors.cardName = "Cardholder's name is required";
+
+    if (!expiryDate) formErrors.expiryDate = "Expiry date is required";
+    else if (!/^(0[1-9]|1[0-2])\/\d{2}$/.test(expiryDate))
+      formErrors.expiryDate = "Expiry date must be in MM/YY format";
+
+    if (!cvv) formErrors.cvv = "CVV is required";
+    else if (cvv.length !== 3) formErrors.cvv = "CVV must be 3 digits";
+
+    setErrors(formErrors);
+
+    if (Object.keys(formErrors).length > 0) {
+      return; // Stop form submission if there are errors
+    }
+
+    setLoading(true);
     const formData = new FormData(e.target);
     const obj = Object.fromEntries(formData.entries());
     const body = JSON.stringify(obj);
-    const { response, status } = await WebHandler(`${URLS.BUYCOURSE}${courseId}`, 'POST', body)
-    if (status === 200) {
-      setSeverity("Success")
-      setShowToast(true)
-      setRes(response.message)
-      setLoading(false)
-      setBuy(true)
-    } else {
-      setSeverity("warning")
-      setShowToast(true)
-      setRes(response.message)
-      setLoading(false)
-    }
-  }
+    const { response, status } = await WebHandler(
+      `${URLS.BUYCOURSE}${courseId}`,
+      "POST",
+      body
+    );
 
+    if (status === 200) {
+      setSeverity("success");
+      setShowToast(true);
+      setRes(response.message);
+      setBuy(true);
+    } else {
+      setSeverity("warning");
+      setShowToast(true);
+      setRes(response.message);
+    }
+    setLoading(false);
+  };
 
   return (
     <>
@@ -60,7 +85,7 @@ const PaymentCard = ({ courseId, course, setShowBuy, setBuy }) => {
                 {course.img ? (
                   <img
                     src={course.img}
-                    // alt={course.tittle}
+                    alt={course.title}
                     className="w-full h-64 object-cover rounded-lg shadow-md"
                   />
                 ) : (
@@ -69,10 +94,10 @@ const PaymentCard = ({ courseId, course, setShowBuy, setBuy }) => {
                   </div>
                 )}
               </div>
-              <h3 className=" text-gray-600 text-start text-xl font-bold">
+              <h3 className="text-gray-600 text-start text-xl font-bold">
                 {course.title}
               </h3>
-              <p className=" text-gray-600 text-start text-xl font-bold">
+              <p className="text-gray-600 text-start text-xl font-bold">
                 ${course.price}
               </p>
             </div>
@@ -92,10 +117,19 @@ const PaymentCard = ({ courseId, course, setShowBuy, setBuy }) => {
                   <input
                     name="cardNumber"
                     id="cardNumber"
-                    type="number"
-                    className="w-full p-4 border border-gray-300 rounded-lg focus:outline-none focus:border-[#0DAFE6]"
+                    type="text"
+                    value={cardNumber}
+                    onChange={(e) => setCardNumber(e.target.value)}
+                    className={`w-full p-4 border ${
+                      errors.cardNumber ? "border-red-500" : "border-gray-300"
+                    } rounded-lg focus:outline-none focus:border-[#0DAFE6]`}
                     placeholder="1234 5678 9012 3457"
                   />
+                  {errors.cardNumber && (
+                    <p className="text-red-500 text-sm mt-1">
+                      {errors.cardNumber}
+                    </p>
+                  )}
                 </div>
                 <div className="mb-6">
                   <label
@@ -108,9 +142,18 @@ const PaymentCard = ({ courseId, course, setShowBuy, setBuy }) => {
                     id="cardName"
                     name="cardName"
                     type="text"
-                    className="w-full p-4 border border-gray-300 rounded-lg focus:outline-none focus:border-[#0DAFE6]"
+                    value={cardName}
+                    onChange={(e) => setCardName(e.target.value)}
+                    className={`w-full p-4 border ${
+                      errors.cardName ? "border-red-500" : "border-gray-300"
+                    } rounded-lg focus:outline-none focus:border-[#0DAFE6]`}
                     placeholder="Name"
                   />
+                  {errors.cardName && (
+                    <p className="text-red-500 text-sm mt-1">
+                      {errors.cardName}
+                    </p>
+                  )}
                 </div>
                 <div className="flex space-x-6 mb-6">
                   <div className="flex-1">
@@ -122,11 +165,20 @@ const PaymentCard = ({ courseId, course, setShowBuy, setBuy }) => {
                     </label>
                     <input
                       id="expiryDate"
-                      type="number"
-                      name="ExData"
-                      className="w-full p-4 border border-gray-300 rounded-lg focus:outline-none focus:border-[#0DAFE6]"
+                      type="text"
+                      name="expiryDate"
+                      value={expiryDate}
+                      onChange={(e) => setExpiryDate(e.target.value)}
+                      className={`w-full p-4 border ${
+                        errors.expiryDate ? "border-red-500" : "border-gray-300"
+                      } rounded-lg focus:outline-none focus:border-[#0DAFE6]`}
                       placeholder="MM/YY"
                     />
+                    {errors.expiryDate && (
+                      <p className="text-red-500 text-sm mt-1">
+                        {errors.expiryDate}
+                      </p>
+                    )}
                   </div>
                   <div className="flex-1">
                     <label
@@ -137,14 +189,24 @@ const PaymentCard = ({ courseId, course, setShowBuy, setBuy }) => {
                     </label>
                     <input
                       id="cvv"
-                      name="CVV"
-                      type="number"
-                      className="w-full p-4 border border-gray-300 rounded-lg focus:outline-none focus:border-[#0DAFE6]"
+                      name="cvv"
+                      type="text"
+                      value={cvv}
+                      onChange={(e) => setCvv(e.target.value)}
+                      className={`w-full p-4 border ${
+                        errors.cvv ? "border-red-500" : "border-gray-300"
+                      } rounded-lg focus:outline-none focus:border-[#0DAFE6]`}
                       placeholder="123"
                     />
+                    {errors.cvv && (
+                      <p className="text-red-500 text-sm mt-1">{errors.cvv}</p>
+                    )}
                   </div>
                 </div>
-                <button className="w-full bg-[#0DAFE6] text-white py-4 rounded-lg font-semibold hover:bg-[#0a94c5] transition-all">
+                <button
+                  type="submit"
+                  className="w-full bg-[#0DAFE6] text-white py-4 rounded-lg font-semibold hover:bg-[#0a94c5] transition-all"
+                >
                   Confirm Payment
                 </button>
               </form>
